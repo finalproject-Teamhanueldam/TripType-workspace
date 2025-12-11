d30d8d0e32d5bc4191525a7b631d067d21d96c78
gdhdhd1024 <bendag1012@gmail.com>
2025-12-11 18:59:20 +0900

    2025.12.11
    수정 초본(메타데이터제외)

---
+++ BackEnd_TripType_Final_Project/src/test/java/com/kh/triptype/TripTypeFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.triptype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTypeFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
