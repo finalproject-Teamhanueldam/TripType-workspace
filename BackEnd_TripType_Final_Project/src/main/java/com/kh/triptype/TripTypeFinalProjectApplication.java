11885d530b3ab56a7f1574a21d844f795bfc4c09
김동윤 <ehddbs4710@naver.com>
2025-12-11 19:23:16 +0900

    25.12.11
    김동윤 푸시

---
+++ BackEnd_TripType_Final_Project/src/main/java/com/kh/triptype/TripTypeFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.triptype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTypeFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripTypeFinalProjectApplication.class, args);
+	}
+
+}
