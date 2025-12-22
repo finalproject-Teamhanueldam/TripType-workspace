694990b01062995bb47589b5c2b4cfc1abeaf0ae
choe kyeong hwan <1chlrudghks@naver.com>
2025-12-22 13:33:11 +0900

    조원들 api 호출 문제 해결(최경환)
    SQL FK 외래키 부분 수정 필요

--- BackEnd_TripType_Final_Project/src/main/java/com/kh/triptype/TripTypeFinalProjectApplication.java
+++ BackEnd_TripType_Final_Project/src/main/java/com/kh/triptype/TripTypeFinalProjectApplication.java
@@ -8,14 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * - 기본 스캔은 "DB 비의존 기능"만 포함
  * - DB 기능은 DbFeatureConfig에서 조건부로 스캔
  */
-@SpringBootApplication(
-    scanBasePackages = {
-        // ✅ DB 안 쓰는 패키지들만
-        "com.kh.triptype.travelAlert",
-        "com.kh.triptype.common"   // 공통 유틸 있으면
-        // ❌ member / admin / statistics 등은 여기 넣지 않음
-    }
-)
+@SpringBootApplication
 public class TripTypeFinalProjectApplication {
 
     public static void main(String[] args) {
